44ae90f2914b0fbdbd2e600eb5be6efe39909747
TheoM4982 <79907392+TheoM4982@users.noreply.github.com>
2021-06-02 16:57:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Batavia Gallery - Shipwreck Museum WA</title> <!-- Batavia Gallery - Shipwreck Museum WA -->
- 
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <link href="css/bootstrap.min.css" rel="stylesheet"> <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script> <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> <![endif]-->
-    <link rel="stylesheet" href="https://use.typekit.net/kur8liw.css">
-    <link rel="stylesheet" href="https://use.typekit.net/njs5oau.css">
-    <!--Website Fonts-->
-    <link rel="stylesheet" href="style.css">
-     <link rel="stylesheet" href="responsive.css" />
-</head>
-<body> 
-<header class="container-fluid nav-top"> <!--Header-->
-
-    <nav class="navbar  navbar-center navbar-inverse navbar-static-top">
-        <div class="container-fluid"> 
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand batavia-title text-center" href="index.html"> <u> Batavia Gallery </u> - <br />Shipwreck Museum WA</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav menu-nav">
-                    <li class="active "><a href="index.html">HOME</a></li>
-                    <li><a href="posts.html">POSTS</a></li>
-                    <li><a href="services.html#booknow">BOOK NOW</a></li>
-                    <li><a href="services.html">EXHIBIT INFORMATION</a></li>
-                    <li><a href="#contact">CONTACT</a></li>
-                </ul>
-
-            </div>
-        </div>
-    </nav>
-</header> <!--Header-->
-
-
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
 <div class="container-fluid ship-bg img-responsive "> <!--Key Image--> 
     <div class="container tagline"> 
     <!--key img and tagline container -->
@@ -72,7 +25,7 @@
     
     <div class="row text-align-justify"> <!--Row-->
         <div class="col-md-4  "> <!--New Exhibit Column-->
-             <h4 class="whats-on-subtitles"> <img src="images/red-square.png" alt="Red Square"> New Exhibit </h4>
+             <h4 class="whats-on-subtitles"> <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/red-square.png" alt="Red Square"> New Exhibit </h4>
             <div class="paragraph whats-on-para"> <!--New Exhibit Paragraph-->
                 
                 <p> The new Batavia shipwreck exhibit has just been installed. The occurrences on board the ship are now made apparent, that the journey was filled with bloodshed and violence among the seafarers due to the dire situation they found themselves in.</p> 
@@ -81,7 +34,7 @@
         </div> <!--New Exhibit Column-->
         
         <div class="col-md-4 pull-img image-center"> <!--Image and pull quote -->
-            <img src=images/ship-whats-on.png class="center" alt="Ship Anchor">
+            <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/ship-whats-on.png" class="center" alt="Ship Anchor">
        
         <div class="text-block "> <!--Pull quote-->
             
@@ -94,7 +47,7 @@
         
         <div class="col-md-4 "> <!--Tour bookings column-->
             
-             <h4 class="whats-on-subtitles"> <img src="images/red-square.png" alt="red square"> Tour Bookings available </h4>
+             <h4 class="whats-on-subtitles"> <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/red-square.png" alt="red square"> Tour Bookings available </h4>
             <div class="paragraph whats-on-para "> <!--Paragraph-->
                 <p> With this school holidays fast approaching..why not book in for a tour? Where you will embark on a journey of discovery and despair and learn about how the crew on the ship ended up in such a situation.</p>
            
@@ -159,7 +112,7 @@
     </div> <!--Address and Contact-->
       
       <div class="col-md-4 ;"> <!--Location Image -->
-          <img src="images/location-img.png" class=" location-img" alt="Front of Museum">
+          <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/location-img.png" class=" location-img" alt="Front of Museum">
           
          
       </div> <!--Location Image -->
@@ -185,73 +138,4 @@
 </div>
  
     </div>  <!--Container -->
-    
-    <footer class="container-fluid footer-img"> 
-        <div class="container">  <!--container-->
-         <div class="row">  <!--row-->
-             <div class="col-md-4">
-                 <h2 class="footer-socialmedia text-center"> <strong> Find us on social media! </strong> </h2>
-                 <div class="footer-icons"> <!--Footer icons-->
-                 <img src="images/websitefacebook.png"   alt="facebook icon">
-                 <img src="images/website%20instagram.png"   alt="instagram icon">
-                 <img src="images/websitelinkedin.png"   alt="linkedin icon" > 
-                 <img src="images/website%20twitter.png"   alt="twitter icon" >
-                 </div> <!--Footer icons-->
-                 </div>
-                 
-             <div class="col-md-4">
-                 
-             </div>
-             
-        <div class="col-md-4 text-center" id="contact" > <h3 class="footer-title"> <u>  Batavia Gallery </u> </h3>
-            <h3 class="footer-title"> <strong> Shipwreck Museum </strong> </h3>
-            
-            <p class="footer-details"><strong> Address: </strong> 47 Cliff St, Fremantle WA 6160</p>
-             <p class="footer-details"> <strong> Phone:</strong> 1300 134 0810</p>
-            <p class="footer-details"> <strong> Email: </strong> <em>  <a class="link" href="mailto:info@washipwrecksmuseum.com.au">  info@washipwrecksmuseum.com.au </a> </em></p>
-            
-                     </div>
-             </div>  <!--row-->
-        </div> <!--container-->
-        <nav class="navbar navbar-inverse navbar-footer">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-     
-    </div>
-    <div class="collapse navbar-collapse navbar-footer-text" id="myNavbar1">
-      <ul class="nav navbar-nav text-center footer-nav ">
-       <li class="footer-txt"><a class="footer-links" href="index.html">HOME</a></li>
-        
-        <li class="footer-txt"><a class="footer-links" href="posts.html">POSTS</a></li>
-        <li class="footer-txt"><a class="footer-links" href="services.html#booknow">BOOK NOW</a></li>
-           <li class="footer-txt"><a class="footer-links "  href="services.html">EXHIBIT INFORMATION</a> </li>
-           <li class="footer-txt"><a class="footer-links" href="index.html#contact">CONTACT</a></li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-          <p class="copyright"> Copyright: WA Shipwrecks Museum </p>
-      
-    </footer>
-    
-    
-        
-    
-    
-    
-    
-  
-
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
