124fe76232d8b8b56d4c80fab1393f703ccaef48
TheoM4982 <79907392+TheoM4982@users.noreply.github.com>
2021-06-03 07:38:36 +0800

    php update
    addedd php for post 1

--- index.php
+++ index.php
@@ -1,141 +1,82 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
-<section class="container-fluid ship-bg img-responsive "> <!--Key Image--> 
-    <div class="container tagline"> 
+<section class="container-fluid blog-bg img-responsive "> <!--Key Image--> 
+    <div class="container posts-tagline"> 
     <!--key img and tagline container -->
     <!--Div that holds the content in the middle of the page-->
-    <h1 class="tagline  text-center"> Discover the newest  shipwreck in WA! </h1>
-    <div class="main-btn  btn btn-lg "> 
-        <!--Button-->
-        <a href="services.html" class="btn-text  "> <strong> Discover More </strong> </a>
-
-    </div><!--Button-->
+        <h1 class="posts-title"> Posts </h1>
+    <h2 class="blog-tagline  text-center"> Find out about the latest insights into the Batavia! </h2>
+    
     <!--the main tag line-->
 
 </div>
     </section> 
 
 <!--key img and tagline container -->
-
-<section class="container-fluid whats-on"> <!--What's On Div -->
-    <div class="container"> 
-    
-    <div class="text-center "> <!--Title -->
-        <h3 class="whats-on-title"> What's On? </h3>
-    </div> <!--Title -->
-    
-    <div class="row text-align-justify"> <!--Row-->
-        <div class="col-md-4  "> <!--New Exhibit Column-->
-             <h4 class="whats-on-subtitles"> <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/red-square.png" alt="Red Square"> New Exhibit </h4>
-            <div class="paragraph whats-on-para"> <!--New Exhibit Paragraph-->
-                
-                <p> The new Batavia shipwreck exhibit has just been installed. The occurrences on board the ship are now made apparent, that the journey was filled with bloodshed and violence among the seafarers due to the dire situation they found themselves in.</p> 
-            </div> <!--New Exhibit Paragraph-->
-            
-        </div> <!--New Exhibit Column-->
-        
-        <div class="col-md-4 pull-img image-center"> <!--Image and pull quote -->
-            <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/ship-whats-on.png" class="center" alt="Ship Anchor">
-       
-        <div class="text-block "> <!--Pull quote-->
-            
-            <p class="pull-quote"> and violence amongst the seafarers due to the dire situation they found themselves in.</p>
-
-        </div> <!--Pull quote-->
-            
-        </div> <!--Image and pull quote -->
-        
-        
-        <div class="col-md-4 "> <!--Tour bookings column-->
-            
-             <h4 class="whats-on-subtitles"> <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/red-square.png" alt="red square"> Tour Bookings available </h4>
-            <div class="paragraph whats-on-para "> <!--Paragraph-->
-                <p> With this school holidays fast approaching..why not book in for a tour? Where you will embark on a journey of discovery and despair and learn about how the crew on the ship ended up in such a situation.</p>
-           
-        </div>  <!--Paragraph-->
-            <div class="whatson-btn " button type="button">
-        <!--Button-->
-        <h1 class="btn-text   "> <strong> Discover More </strong> </h1>
-
-    </div><!--Button-->
-    <!--the main tag line-->
-            
-    </div> <!--Tour bookings column-->
-    </div> <!--Row-->
-    </div> <!--What's On Div -->
-    </section>
-
-<section class="container-fluid img-responsive testimonials"> <!--Testimonials -->
-    <h1 class="text-center testimonials-title"> Testimonials</h1>
-   <div class="container">
-  <div class="row">
-
-  <div class="row">
-      
-    <div class="col-md-4"> <!--Belinda quote-->
-        
-        <p class="testimonials-text"> What a great family experience for people of any age, with many interesting artefacts from a time long forgotten by many.</p>
-        <h2 class="text-center testimonials-names"> Belinda</h2>
-    </div> <!--Belinda quote--> 
-      
-      <div class="col-md-4"> <!--Steve quote-->
-          
-          <p class="testimonials-text"> A super interesting and informative experience! Such significant detail and depth is delved into, will have to reccomend to others </p>
-     <h2 class="text-center testimonials-names"> Steve</h2>
-      </div> <!--Steve quote--> 
-      
-      <div class="col-md-4"> <!--Greg quote-->
-          
-          <p class="testimonials-text"> It was very intriguing to learn about all the bloodshed and horror that occurred on the ship. To think that such a thing happened in WA is surreal. </p>
-          <h2 class="text-center testimonials-names"> Greg</h2>
-      </div> <!--Greg quote-->
+    <section class="container-fluid posts-bg">  <!--Posts-->
+    <div class="container">  <!--Posts Container--
    
-  </div>
-
-</div>
- 
-    </div> 
-    </section> <!--Testimonials -->
-    
-    
-    <section class="container-fluid  location"> <!--Location -->
-    <h1 class="text-center location-title"> Location </h1>
-   <div class="container"> <!--Container -->
-  <div class="row">
-
-  <div class="row">
-      
-    <div class="col-md-4"> <!--Address and Contact-->
         
-       <p class="museum-details"> <strong> Address:</strong> <em>  47 Cliff St, Fremantle WA 6160 </em> </p>
-        <p class="museum-details"> <strong> Open:</strong>   9:30AM-5:00 PM  <em>Mon- Fri </em> </p>
-        <p class="museum-details"> <strong> Phone:</strong>   1300 134 081</p>
-    </div> <!--Address and Contact-->
-      
-      <div class="col-md-4 ;"> <!--Location Image -->
-          <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/location-img.png" class=" location-img" alt="Front of Museum">
-          
-         
-      </div> <!--Location Image -->
-      
-      <div class="col-md-4 text-center"> <!--Greg quote-->
-          <p> <em> Located right on the edge of Fremantle </em>
-
-
-</p>
-          <p> <em> Right next to Bather’s Beach and Notre Dame University </em>
-              </p>
-               <div class="directions-btn text-center" button type="button">
-            
-        <!--Button-->
-<a href="https://www.google.com/maps/dir//47+Cliff+St,+Fremantle+WA+6160/@-32.0572338,115.7402089,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x2a32a171b29aead5:0x90971f6b788b8166!2m2!1d115.7424029!2d-32.0572338" class="btn-text"> <strong>Directions </strong> </a> 
-        </div>
-    </div><!--Button-->
+  <div class="row"> <!--Exhibit Row-->
+    <div class="col-md-8"> <!--New Exhibit Col-->
         
-      </div> <!--Greg quote-->
-   
-  </div>
+        
+<?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=22');//look for posts that have the category of 2
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 22 get thet title and content
+?>
+<h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4> 
+<div><p class="post-para"><?php the_content() ?></p></div>
+<?php
+endwhile;
+endif;
+wp_reset_query();?>
+      
+      
+      
+      
+      </div> <!--New Exhibit Col-->
+      
+    <div class="col-md-4"> <!--New Exhibit Col Img-->
+      <img class="post-imgs" src="images/new-exhibit.jpg" alt="ship at exhibit">
+      </div> <!--!--New Exhibit Col Img-->-->
+  </div> <!--Exhibit Row-->
+        <h3 class="post-titles"> Older Posts</h3>
+ <div class="row older-posts"> <!--!--Older posts row-->-->
+    <div class="col-md-8"> <!--Weapons Col-->
+        
+        
+        <h4 class="post-old"> What weapons were used? </h4>
+            <p class="post-para-old"> Times are tough when it's everyone out on the wild sea. How did these seafarers defend themselves against others and the vast terrors in the wide embrace of the sea?</p>
+        <h4 class="read-more"> Read More</h4>
+      
+      
+      
+      
+      </div> <!--Weapons Col-->
+    <div class="col-md-4"> <!--Cannon image-->
+      <img class="post-imgs" src="images/cannon.jpg" alt="cannon">
+      </div> <!--Cannon image-->
+  </div> <!--Older posts row-->
 
-</div>
- 
-    </section>  <!--Container -->
+        <div class="row"> <!--!--Row-->--
+    <div class="col-md-8 material-col"> <!--Delving into the material col-->
+        
+        
+        <h4 class="post-old"> Delving into the material </h4>
+            <p class="post-para-old"> Find out exactly what materials they used on these ships, with some of the common uses ranging from a variety of things including bandaging...</p>
+        <h4 class="read-more"> Read More</h4>
+      
+      
+      
+      
+      </div> <!--Delving into the material col-->
+    <div class="col-md-4 material-col "> <!--Materials Col-->
+      <img  class="post-imgs" src="images/materials.jpg" alt="materials">
+      </div> <!--Materials Col-->
+  </div> <!--Row-->
+        </div> <!--Posts Container-->
+    </section> <!--Posts-->
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
