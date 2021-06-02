2a0704ca062450f1cc3db9d35bc6c52ab164a606
TheoM4982 <79907392+TheoM4982@users.noreply.github.com>
2021-06-02 16:57:13 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,55 @@
+<footer class="container-fluid footer-img"> 
+        <div class="container">  <!--container-->
+         <div class="row">  <!--row-->
+             <div class="col-md-4">
+                 <h2 class="footer-socialmedia text-center"> <strong> Find us on social media! </strong> </h2>
+                 <div class="footer-icons"> <!--Footer icons-->
+                 <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/websitefacebook.png"   alt="facebook icon">
+                 <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/website%20instagram.png"   alt="instagram icon">
+                 <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/websitelinkedin.png"   alt="linkedin icon" > 
+                 <img src="http://206.189.45.97/~mesh16/wp-content/themes/Batavia-Site-/images/website%20twitter.png"   alt="twitter icon" >
+                 </div> <!--Footer icons-->
+                 </div>
+                 
+             <div class="col-md-4">
+                 
+             </div>
+             
+        <div class="col-md-4 text-center" id="contact" > <h3 class="footer-title">
+<?php bloginfo('name'); ?>
+        </h3>
+            
+            <p class="footer-details"><strong> Address: </strong> 47 Cliff St, Fremantle WA 6160</p>
+             <p class="footer-details"> <strong> Phone:</strong> 1300 134 0810</p>
+            <p class="footer-details"> <strong> Email: </strong> <em>  <a class="link" href="mailto:info@washipwrecksmuseum.com.au">  info@washipwrecksmuseum.com.au </a> </em></p>
+            
+                     </div>
+             </div>  <!--row-->
+        </div> <!--container-->
+        <nav class="navbar navbar-inverse navbar-footer">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+     
+    </div>
+    <div class="collapse navbar-collapse navbar-footer-text" id="myNavbar1">
+      <ul class="nav navbar-nav text-center footer-nav ">
+       <li class="footer-txt"><a class="footer-links" href="index.html">HOME</a></li>
+        
+        <li class="footer-txt"><a class="footer-links" href="posts.html">POSTS</a></li>
+        <li class="footer-txt"><a class="footer-links" href="services.html#booknow">BOOK NOW</a></li>
+           <li class="footer-txt"><a class="footer-links "  href="services.html">EXHIBIT INFORMATION</a> </li>
+           <li class="footer-txt"><a class="footer-links" href="index.html#contact">CONTACT</a></li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+          <p class="copyright"> Copyright: WA Shipwrecks Museum </p>
+      
+    </footer>
+<?php wp_footer(); ?>
