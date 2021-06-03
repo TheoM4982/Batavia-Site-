eefca0fe473e2252d7a6a15e4792c6daf1588590
TheoM4982 <79907392+TheoM4982@users.noreply.github.com>
2021-06-03 12:21:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,18 +15,19 @@
 <!--key img and tagline container -->
     <section class="container-fluid posts-bg">  <!--Posts-->
     <div class="container">  <!--Posts Container-->
-<?php $post_id = 895;
+<?php $post_id = 1043;
 $queried_post = get_post($post_id);?>
+<h3 class="post-titles"><?php echo $queried_post->post_title; ?></h3>
        
         <div class="row"> <!--Exhibit Row-->
-    <div class="col-md-8"> <!--New Exhibit Col-->
+    
         
         
-<div class="col-md-4">
+<div class="col-md-8">
 <?php $post_id = 895;
 $queried_post = get_post($post_id);?>
 <h4 class="post-new"><?php echo $queried_post->post_title; ?></h4>
-<div><p class="post-para"><?php the_content() ?> </div>
+<p class="post-para"><?php the_content() ?>
 <?php echo $queried_post->post_excerpt; ?>
 <?php echo '<a class="read-more" href="'.get_permalink($queried_post).'"><br>Read More</a>';?>                      
 
@@ -34,62 +35,58 @@ $queried_post = get_post($post_id);?>
       
       
       
-      </div> <!--New Exhibit Col-->
+     
       
     <div class="col-md-4 post-imgs "> 
      <?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?><!--New Exhibit Col Img-->
       
-      </div> <!--!--New Exhibit Col Img-->-->
+      </div> <!--!--New Exhibit Col Img-->
   </div> <!--Exhibit Row-->
-        <h3 class="post-titles"> Older Posts</h3>
+        
  <div class="row older-posts"> <!--!--Older posts row-->-->
+
     <div class="col-md-8"> <!--Weapons Col-->
+<h3 class="post-titles"> Older Posts</h3>
 <?php $post_id = 927;
 $queried_post = get_post($post_id);?>
 <h4 class="post-old"><?php echo $queried_post->post_title; ?></h4>
-<div><p class="post-para"><?php the_content() ?>
+<p class="post-para"><?php the_content() ?>
 <?php echo $queried_post->post_excerpt; ?>
 <?php echo '<a class="read-more" href="'.get_permalink($queried_post).'"><br>Read More</a>';?>
             
-      
+      </div>
       
       
 
       
     <div class="col-md-4">
-<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?> <!--Cannon image-->
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive text-center post-img')); ?> <!--Cannon image-->
       
       </div> <!--Cannon image-->
-  </div> <!--Older posts row-->
+   <!--Older posts row-->
 
         <div class="row"> <!--!--Row-->--
     <div class="col-md-8 material-col"> <!--Delving into the material col-->
         
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=26');//look for posts that have the category of 26
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 26 get thet title and content
-?>
-<h4 class="post-old"><?php the_title();?></h4>
-<div><p class="post-para-old"><?php the_content() ?></p></div>
-<h4 class="read-more"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>
-      
+<?php $post_id = 985;
+$queried_post = get_post($post_id);?>
+
+<h4 class="post-old"><?php echo $queried_post->post_title; ?></h4>
+<p class="post-para-old"><?php the_content() ?>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="read-more" href="'.get_permalink($queried_post).'"><br>Read More</a>';?>
       
       
       </div> <!--Delving into the material col-->
     <div class="col-md-4 material-col "> <!--Materials Col-->
-      
-      </div> <!--Materials Col-->
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive text-center post-img')); ?> <!--Cannon image-->
+</div> <!--Materials Col-->
   </div> <!--Row-->
         </div> <!--Posts Container-->
         </div>
-        </div>
-
+      
+    
+     
+      
     </section> <!--Posts-->
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
